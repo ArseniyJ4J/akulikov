36b34d864ab97b89ca7a4aa23f6a93e5bfe4d190
ArseniyJ4J <arsmail424@gmail.com>
2017-04-24 13:49:23 +0300

    1.3 JavaDoc. Code convention. (addition file packag-info.java and JavaDocs in Calculate.java)

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,16 @@
-package ru.job4j;
-
+package akulikov.chapter_001.src.main.java.ru.job4j;
+/**
+ * Class Класс для будущего выполнения арифметических вычислений +, -, /, */ ^/ и так далее.
+ * @author akulikov
+ * @since 24.04.2017
+ * @version 1
+ */
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
-
+	/**
+	* Пока вычислять нечего, поэтому ввел метод,
+	* который выводит на экран консоли предложение "Hello world!"
+	*/
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task
+* 
+* @author Arseniy Kulikov (mailto:arsmail424@gmail.com)
+* @version 1.0
+* @since 1.0
+*/
+
+package akulikov.chapter_001.src.main.java.ru.job4j;
