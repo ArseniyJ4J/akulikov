286092c2dfb2a4b96b1722c1b1cff71017f3de8e
ArseniyJ4J <arsmail424@gmail.com>
2017-04-29 22:46:29 +0300

    3.2 Проверка положения точки на прямой

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+/** Class Класс для проверки принадлежность координат точки к функции прямой.
+* @author Arseniy Kulkiov
+* @since 29.04.2017
+* @version 1
+*/
+public class Point {
+	/**
+	* Объявление переменной x в поле класса.
+	* @param x - "x" coordinate.
+	*/
+	private int x;
+		/**
+	* Объявление переменной y в поле класса.
+	* @param y - "y" coordonate.
+	*/
+	private int y;
+	/**
+	* Метод присвоения значений переменным x,y.
+	* @param x - "x" coordinate.
+	* @param y - "y" coordonate.
+	*/
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	* Метод возвращения значения.
+	* @return - возвращение.
+	*/
+	public int getX() {
+		return this.x;
+	}
+	/**
+	* Метод возвращения значения.
+	* @return - возвращение.
+	*/
+	public int getY() {
+		return this.y;
+	}
+	/**
+	* Метод определения принадлежности точки к прямой.
+	* @return - возвращение.
+	* @param a - постоянная a.
+	* @param b - постоянная b.
+	*/
+	public boolean is(int a, int b) {
+		return this.getY() == a * this.getX() + b;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+* Test por Ppint.
+*
+* @author Arseniy Kulikov (mailto:arsmail424@gmail.com)
+* @version 1.0
+* @since 1.0
+*/
+public class PointTest {
+	/**
+	* Test.
+	*/
+	@Test
+	public void pointOnLine() {
+		Point point = new Point(6, 15);
+		boolean result = point.is(2, 3);
+		assertThat(result, is(true));
+	}
+}
