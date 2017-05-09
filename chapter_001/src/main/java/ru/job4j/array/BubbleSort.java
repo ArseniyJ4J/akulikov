602f3c49b527597292001fe4528f6d908782c84a
ArseniyJ4J <arsmail424@gmail.com>
2017-05-09 20:23:46 +0300

    5.1 Bubble Sort

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -13,21 +13,15 @@ public class BubbleSort {
 	*/
 	public int[] sort(int[] array) {
 	int limit = array.length - 1;
-	//pointBreak:
 	for (int i1 = limit; i1 >= 1; i1--) {
 		int i;
 		for (i = 0; i < limit;  i++) {
 			int buffer;
 			if (array[i] > array[i + 1]) {
-			//limit = limit - 1;
-			//break pointBreak;
-			//} else {
 				buffer = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = buffer;
 			}
-			//;
-			//break pointBreak;
 		}
 	}
 	return array;
