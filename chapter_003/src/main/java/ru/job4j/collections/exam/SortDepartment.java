9929df179a6378c68be2dc33bdf906938e399b7d
ArseniyJ4J <arsmail424@gmail.com>
2017-08-15 17:19:30 +0300

    Тестовое задание. Не получается компаратор по убыванию с сохранением иерархии.

--- chapter_003/src/main/java/ru/job4j/collections/exam/SortDepartment.java
+++ chapter_003/src/main/java/ru/job4j/collections/exam/SortDepartment.java
@@ -4,21 +4,46 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.lang.Character.isDigit;
+
 /** Class Класс SortDepartment.
  * @author Arseniy Kulkiov
  * @since 14.08.2017
  * @version 1
  */
+
 public class SortDepartment {
-
-    public String[] departments = new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
-
-    public List<String> list = new ArrayList<String>();
-
-    public void loadDepartment(String[] stringArray) {
-        for (String string : stringArray) {
-            if (!this.list.contains(string)) {
-                this.list.add(string);
+    /**
+     * Поле класса - массив со входящими значениями.
+     */
+    private String[] entryData = new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+    /**
+     * Поле класса - list с преобразованными входящими значениями в виде множества неповторяющихся объектов Department.
+     */
+    private List<Department> departments = new ArrayList<>();
+    /**
+     * Геттер для поля entryData.
+     * @return - возврат значения.
+     */
+    public String[] getEntryData() {
+        return entryData;
+    }
+    /**
+     * Геттер для поля departments.
+     * @return - возврат значения.
+     */
+    public List<Department> getDepartments() {
+        return departments;
+    }
+    /**
+     * Метод загружающий входящие значения в виде множества объектов Department в List<Department> departments.
+     * @param entryData - входящий значения.
+     */
+    public void loadDepartment(String[] entryData) {
+        List<String> list = new ArrayList<>();
+        for (String string : entryData) {
+            if (!list.contains(string)) {
+                list.add(string);
             }
             StringBuilder sb = new StringBuilder(string);
             int index = 0;
@@ -27,65 +52,65 @@ public class SortDepartment {
                 index = delimiter;
                 if (delimiter != -1) {
                     String subString = sb.substring(0, delimiter);
-                    if (!this.list.contains(subString)) {
-                        this.list.add(subString);
+                    if (!list.contains(subString)) {
+                        list.add(subString);
                     }
                 }
             }
         }
+        for (String string : list) {
+            ArrayList<Integer> result = new ArrayList<>();
+            for (int i = 0; i < string.length(); i++) {
+                if (isDigit(string.charAt(i))) {
+                    result.add(Integer.parseInt(Character.toString(string.charAt(i))));
+                }
+            }
+            departments.add(new Department(string, result));
+        }
     }
-
-    public List<String> ascendingSort(List<String> list) {
-        list.sort(new Comparator<String>() {
+    /**
+     * Метод сортирующий департаменты по возрастанию.
+     * @param list - сортируемая коллекция.
+     * @return - возврат значения.
+     */
+    public List<Department> ascendingSort(List<Department> list) {
+        list.sort(new Comparator<Department>() {
             @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
+            public int compare(Department o1, Department o2) {
+                return o1.getDepartment().compareTo(o2.getDepartment());
             }
         });
         return list;
     }
-
-    public List<String> descendingSort(List<String> list) {
-        list.sort(new Comparator<String>() {
+    /**
+     * Метод сортирующий департаменты по убыванию с сохранением иерархии.
+     * @param list - сортируемая коллекция.
+     * @return - возврат значения.
+     */
+    public List<Department> descendingSort(List<Department> list) {
+        this.ascendingSort(list);
+        list.sort(new Comparator<Department>() {
             @Override
-            public int compare(String o1, String o2) {
-                int result = o2.compareTo(o1);
-//                =================================================
-//                int result = 0;
-//                if (o2.contains(o1)) {
-//                    result = o2.compareTo(o1);
-//                }
-//===============================================================
-//                int result = o2.compareTo(o1);
-//                if (result == -1) {
-//                    result = o2.contains(o1) ? -1 : 1;
-//                }
-//                int result = o1.contains(o2) ? 1 : -1;
-//                System.out.println(result);
-//                if (result == -1) {
-//                    result = o2.compareTo(o1);
-//                }
+            public int compare(Department o1, Department o2) {
+                int result = 1;
+                if (o1.getCode().size() == o2.getCode().size()) {
+                    for (int i = 0; i < o1.getCode().size(); i++) {
+                        if (o1.getCode().get(i) > o2.getCode().get(i)) {
+                            result = -1;
+                            break;
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < Integer.min(o1.getCode().size(), o2.getCode().size()); i++) {
+                        if (o1.getCode().get(i) > o2.getCode().get(i)) {
+                            result = -1;
+                            break;
+                        }
+                    }
+                }
                 return result;
             }
         });
         return list;
-    }
-
-    public static void main(String[] args) {
-        SortDepartment sd = new SortDepartment();
-        sd.loadDepartment(sd.departments);
-        for (String string : sd.list) {
-            System.out.println(string);
-        }
-        System.out.println("***********************************************");
-        sd.ascendingSort(sd.list);
-        for (String string : sd.list) {
-            System.out.println(string);
-        }
-        System.out.println("***********************************************");
-        sd.descendingSort(sd.list);
-        for (String string : sd.list) {
-            System.out.println(string);
-        }
     }
 }
--- chapter_003/src/test/java/ru/job4j/collections/exam/SortDepartmentTest.java
+++ chapter_003/src/test/java/ru/job4j/collections/exam/SortDepartmentTest.java
@@ -2,7 +2,8 @@ package ru.job4j.collections.exam;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /** Class Класс SortDepartmentTest.
  * @author Arseniy Kulkiov
@@ -11,18 +12,48 @@ import static org.junit.Assert.*;
  */
 
 public class SortDepartmentTest {
+    /**
+     * Тест.
+     */
     @Test
     public void loadDepartment() {
         SortDepartment sd = new SortDepartment();
-        sd.loadDepartment(sd.departments);
+        sd.loadDepartment(sd.getEntryData());
+        String expected = ", K1\\SK1, K1, K1\\SK2, K1\\SK1\\SSK1, K1\\SK1\\SSK2, K2, K2\\SK1\\SSK1, K2\\SK1, K2\\SK1\\SSK2";
+        String result = "";
+        for (Department d : sd.getDepartments()) {
+            result = String.format("%s, %s", result, d.getDepartment());
+        }
+        assertThat(result, is(expected));
     }
-
+    /**
+     * Тест.
+     */
     @Test
-    public void ascendingSort() throws Exception {
+    public void ascendingSort() {
+        SortDepartment sd = new SortDepartment();
+        sd.loadDepartment(sd.getEntryData());
+        sd.ascendingSort(sd.getDepartments());
+        String expected = ", K1, K1\\SK1, K1\\SK1\\SSK1, K1\\SK1\\SSK2, K1\\SK2, K2, K2\\SK1, K2\\SK1\\SSK1, K2\\SK1\\SSK2";
+        String result = "";
+        for (Department d : sd.getDepartments()) {
+            result = String.format("%s, %s", result, d.getDepartment());
+        }
+        assertThat(result, is(expected));
     }
-
+    /**
+     * Тест.
+     */
     @Test
-    public void descendingSort() throws Exception {
+    public void descendingSort() {
+        SortDepartment sd = new SortDepartment();
+        sd.loadDepartment(sd.getEntryData());
+        sd.descendingSort(sd.getDepartments());
+        String expected = ", K2, K2\\SK1, K2\\SK1\\SSK2, K2\\SK1\\SSK1, K1, K1\\SK2, K1\\SK1, K1\\SK1\\SSK2, K1\\SK1\\SSK1";
+        String result = "";
+        for (Department d : sd.getDepartments()) {
+            result = String.format("%s, %s", result, d.getDepartment());
+        }
+        assertThat(result, is(expected));
     }
-
 }
