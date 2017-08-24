7a93976924a1b023a57ea890dfb8fdec7432e142
ArseniyJ4J <arsmail424@gmail.com>
2017-08-24 13:48:40 +0300

    5.2.2. Реализовать Store<T extends Base> [#157] Исправление. Добавил BaseStore.

--- chapter_005/src/main/java/ru/job4j/collectionspro/generic/store/UserStore.java
+++ chapter_005/src/main/java/ru/job4j/collectionspro/generic/store/UserStore.java
@@ -1,7 +1,5 @@
 package ru.job4j.collectionspro.generic.store;
 
-import ru.job4j.collectionspro.generic.SimpleArray;
-
 /** Класс UserStore.
  * @author Arseniy Kulkiov
  * @since 21.08.2017
@@ -9,57 +7,12 @@ import ru.job4j.collectionspro.generic.SimpleArray;
  * @param <T> - параметризованный тип.
  */
 
-public class UserStore<T extends User> implements Store<T> {
-    /**
-     * Поле класса.
-     */
-    private SimpleArray<T> sa;
-    /**
-     * Поле класса.
-     */
-    private int size;
+public class UserStore<T extends User> extends BaseStore<T> {
     /**
      * Конструктор.
      * @param size - размер хранилища.
      */
     public UserStore(int size) {
-        this.sa = new SimpleArray<T>(size);
-        this.size = size;
-    }
-    /**
-     * Геттер для поля класса sa(хранилища).
-     * @return - возврат значения.
-     */
-    public SimpleArray<T> getSa() {
-        return sa;
-    }
-    /**
-     * Метод добавления.
-     * @param value - добавляемый объект.
-     */
-    @Override
-    public void add(T value) {
-        this.sa.add(value);
-    }
-    /**
-     * Метод обновления.
-     * @param value - обновляемый объект.
-     */
-    @Override
-    public void update(T value) {
-        for (int i = 0; i < this.size; i++) {
-            if (this.sa.get(i).getId().equals(value.getId())) {
-                this.sa.update(value, i);
-                break;
-            }
-        }
-    }
-    /**
-     * Метод удаления.
-     * @param value - удаляемый объект.
-     */
-    @Override
-    public void remove(T value) {
-        this.sa.delete(value);
+        super(size);
     }
 }
