a4c552a58efff0f4e7066923a9d846c4d5b27dae
ArseniyJ4J <arsmail424@gmail.com>
2018-04-02 19:57:18 +0300

    Парсер вакансий на sql.ru [#1731][#45442] Исправление теста

--- chapter_003_2/src/test/java/ru/job4j/sql/jsouparsing/RegExTest.java
+++ chapter_003_2/src/test/java/ru/job4j/sql/jsouparsing/RegExTest.java
@@ -23,27 +23,13 @@ public class RegExTest {
      */
     @Test
     public void test1() {
-        List<String> list = new ArrayList<>();
-
-        list.add("Java");
-        list.add("java");
-        list.add(" Java ");
-        list.add(" java ");
-        list.add(" Java");
-        list.add("Java ");
-        list.add("JAVA");
-        list.add("Ищем java-разработчиков (1,2,3,все)");
-        list.add("Ведущий Java разработчик (г.Москва)");
-        list.add("Открыта вакансия Java-разработчика\t");
-        list.add("Java программист на удаленку");
-        list.add("Ищем Full Stack Java Developer (удаленно, долгосрочный проект)");
-
+        String[] array = {"Java", "java", " Java ", " java ", " Java", "Java ", "JAVA", "Ищем java-разработчиков (1,2,3,все)", "Ведущий Java разработчик (г.Москва)", "Открыта вакансия Java-разработчика\t", "Java программист на удаленку", "Ищем Full Stack Java Developer (удаленно, долгосрочный проект)"};
         String wrong = "Java Script";
         String regINclus = "(?i)(^|.+)java(.+|$)";
         String regEXclus = "(?i)(^|.+)script(.+|$)";
         Pattern patIN = Pattern.compile(regINclus);
         Pattern patEX = Pattern.compile(regEXclus);
-        for (String s : list) {
+        for (String s : array) {
             Matcher matchIN = patIN.matcher(s);
             Matcher matchEX = patEX.matcher(s);
             boolean result = (!matchEX.matches() && matchIN.matches());
@@ -56,28 +42,20 @@ public class RegExTest {
         Assert.assertThat(result, is(false));
     }
 
-
     /**
      * Test.
      */
     @Test
     public void test2() {
-        List<String> list = new ArrayList<>();
-        list.add("http://www.sql.ru/forum/1284926/ishhem-java-razrabotchikov");
-        list.add("http://www.sql.ru/forum/1287266/ishhem-full-stack-java-developer-udalenno-dolgosrochnyy-proekt");
-        list.add("http://www.sql.ru/forum/1287054/vedushhiy-java-razrabotchik-g-moskva");
-        list.add("http://www.sql.ru/forum/1287009/otkryta-vakansiya-java-razrabotchika");
-        list.add("http://www.sql.ru/forum/1286858/java-programmist-na-udalenku");
-
+        String[] array = {"http://www.sql.ru/forum/1284926/ishhem-java-razrabotchikov", "http://www.sql.ru/forum/1287266/ishhem-full-stack-java-developer-udalenno-dolgosrochnyy-proekt", "http://www.sql.ru/forum/1287054/vedushhiy-java-razrabotchik-g-moskva", "http://www.sql.ru/forum/1287009/otkryta-vakansiya-java-razrabotchika", "http://www.sql.ru/forum/1286858/java-programmist-na-udalenku"};
         String wrong = "http://www.sql.ru/forum/memberinfo.aspx?mid=247585";
         String regINclus = "^http://www.sql.ru/forum/[0-9]{5,10}(.+|$)";
         Pattern patIN = Pattern.compile(regINclus);
-        for (String s : list) {
+        for (String s : array) {
             Matcher matchIN = patIN.matcher(s);
             boolean result = matchIN.matches();
             Assert.assertThat(result, is(true));
         }
-
         Matcher matchIN = patIN.matcher(wrong);
         boolean result = matchIN.matches();
         Assert.assertThat(result, is(false));
